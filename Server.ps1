9c65c28eaa499186b949242811de9c2678e91859
Tosslog-admin <47279146+Tosslog-admin@users.noreply.github.com>
2022-09-29 20:38:11 +0800

    Add files via upload

--- Server.ps1
+++ Server.ps1
@@ -1,9 +1,22 @@
-$VENV_PATH= $PSScriptRoot+'\_venv'
+
+$JsonContent = Get-Content -Encoding "UTF8" -Path $PSScriptRoot"\config\config.json" -Raw | ConvertFrom-Json 
+$APP_NAME = $JsonContent.APP_NAME
+
+$VENV_PATH= $PSScriptRoot+"\_venv"
 $TRUE_FALSE= (Test-Path $VENV_PATH)
 $createvenv= $true
 if($TRUE_FALSE -ne "True")
 {
     python -m venv _venv
 }
+
+$VENV_PATH= $PSScriptRoot+"\_venv"
+$TRUE_FALSE= (Test-Path $VENV_PATH)
+$createvenv= $true
+if($TRUE_FALSE -eq "True")
+{
+    Copy-Item -Path $VENV_PATH'\scripts\'python.exe -Destination $VENV_PATH'\scripts\'$APP_NAME""_python.exe
+}
 _venv/scripts/activate.ps1 
-python main.py
+$s = $APP_NAME+"_python.exe main.py"
+Invoke-Expression $s
